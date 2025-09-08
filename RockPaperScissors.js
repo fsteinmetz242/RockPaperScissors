b69608322551a9a1f28928d4f0d6d90dbf027c10
Frank Steinmetz <frank.steinmetz@steinmetz-it.de>
2025-09-08 11:37:05 +0200

    Lauffähige Version

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,24 +1,32 @@
 // Player should give his draw from command line
 
-function getResult(sHuman='', cpuChoose){
+function getResult(sHuman, cpuChoose){
 let result = '';
+let wahlSpieler='';
+
+console.log('Mensch: ' + sHuman);
+
     if(sHuman.toLowerCase() === 'rock'){
+        console.log('rock');
         // stein o papier - schere +
         cpuChoose === 'rock' ? result='no winner' :         cpuChoose === 'paper' ? result='you win' :                 result = 'computer wins';
     }
     if(sHuman.toLowerCase() === 'paper'){
+                console.log('paper');
         // stein + papier o schere -
         cpuChoose === 'rock' ? result='computer wins' :
         cpuChoose === 'paper' ? result='no winner' :
-                result = 'computer wins';
+                result = 'you win';
     }
 
     if(sHuman.toLowerCase() === 'scissors'){
+                console.log('sci');
         // stein - papier + schere o
         cpuChoose === 'rock' ? result='computer wins' :
         cpuChoose === 'paper' ? result='you win' :
                 result = 'no winner';
     }
+
     return result;
 }
 
@@ -28,7 +36,7 @@ let myChoose = '';
 let sHuman = '';
 const argv = process.argv.slice(2);
 console.log(argv);
-sHuman = argv[2];
+sHuman = argv[0];
 
 myChoose = myChooses[Math.floor(Math.random()*myChooses.length)];
 console.log(`You choose: ${argv[0]} and my choose: ${myChoose}. Result ${getResult(sHuman,myChoose)} `);
