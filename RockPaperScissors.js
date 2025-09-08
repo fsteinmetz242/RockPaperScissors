293a7d2abeb0c582d20812263e740eb230e689be
Frank Steinmetz <frank.steinmetz@steinmetz-it.de>
2025-09-08 14:38:44 +0200

    Variablen bereinigt

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,62 +1,63 @@
 // Player should give his draw from command line
 
-function getResult(sHuman, cpuChoose) {
+function getResult(humanChoose, computerChoose) {
   let result = "";
 
-  if (sHuman === "rock") {
-    console.log("rock");
+  if (humanChoose === "rock") {
     // stein o papier - schere +
-    cpuChoose === "rock"
+    computerChoose === "rock"
       ? (result = "no winner")
-      : cpuChoose === "paper"
+      : computerChoose === "paper"
       ? (result = "you win")
       : (result = "computer wins");
   }
-  if (sHuman === "paper") {
-    console.log("paper");
+  if (humanChoose === "paper") {
     // stein + papier o schere -
-    cpuChoose === "rock"
+    computerChoose === "rock"
       ? (result = "computer wins")
-      : cpuChoose === "paper"
+      : computerChoose === "paper"
       ? (result = "no winner")
       : (result = "you win");
   }
 
-  if (sHuman === "scissors") {
-    console.log("sci");
+  if (humanChoose === "scissors") {
     // stein - papier + schere o
-    cpuChoose === "rock"
+    computerChoose === "rock"
       ? (result = "computer wins")
-      : cpuChoose === "paper"
+      : computerChoose === "paper"
       ? (result = "you win")
       : (result = "no winner");
   }
   return result;
 }
 
-const myChooses = ["rock", "paper", "scissor"];
+const availableChooses = ["rock", "paper", "scissor"];
 // Eingabe prüfen Eingabe => lowercase!!
-let myChoose = "";
-let sHuman = "";
+let computerChoose = "";
+let humanChoose = "";
 
 const argv = process.argv.slice(2); // Entferne zwei Elemente
 // Arg1 = The path to the Node.js executable (your installation of Node)
 // Arg2 = The path to the JavaScript file being executed (your file)
+// Jeder Eintrag ist ein String!
 if (process.argv.length < 3) {
   console.log("Keine Auswahl getroffen! Bitte erneut versuchen.");
-  return;
+  process.exit(1);
 }
 
-sHuman = argv[0].toLowerCase();
-if (!myChooses.includes(sHuman)) {
-  console.log("Falsche Eingabe. Erlaubt sind nur " + myChooses.join(" | "));
-  return;
+humanChoose = argv[0].toLowerCase();
+if (!availableChooses.includes(humanChoose)) {
+  console.log(
+    "Falsche Eingabe. Erlaubt sind nur " + availableChooses.join(" | ")
+  );
+  process.exit(1);
 }
 
-myChoose = myChooses[Math.floor(Math.random() * myChooses.length)];
+computerChoose =
+  availableChooses[Math.floor(Math.random() * availableChooses.length)];
 console.log(
-  `You choose: ${argv[0]} and my choose: ${myChoose}. Result ${getResult(
-    sHuman,
-    myChoose
+  `You choose: ${argv[0]} and my choose: ${computerChoose}. Result ${getResult(
+    humanChoose,
+    computerChoose
   )} `
 );
