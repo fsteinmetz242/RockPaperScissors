087cf3d8d5cbce0bd93f33a2ae62d38069a95f9f
Frank Steinmetz <frank.steinmetz@steinmetz-it.de>
2025-09-11 17:32:29 +0200

    Funktion verabeiten eines Wortes erstellt

--- PigLatinTranslator.js
+++ PigLatinTranslator.js
@@ -1,13 +1,4 @@
 // Testsatz: There are other Happy fish in the sea Child
-const argv = process.argv.slice(2); // Entferne zwei Elemente
-// Arg1 = The path to the Node.js executable (your installation of Node)
-// Arg2 = The path to the JavaScript file being executed (your file)
-// Jeder Eintrag ist ein String!
-if (process.argv.length < 3) {
-  console.log("Keine Eingabe gemacht! Bitte erneut versuchen.");
-  process.exit(1);
-}
-
 // Regeln:
 // Wenn ein Wort mit einem Konsonanten und einem Vokal beginnt, setze den ersten Buchstaben des Wortes an das Ende des Wortes und füge „ay“ hinzu.
 //      Happy = appyh + ay = appyhay
@@ -20,46 +11,52 @@ if (process.argv.length < 3) {
 // Dann für jedes Wort prüfen ob eine Regel zutrifft. Falls ja anwenden.
 // Zuletzt dann wieder String erstellen und ausgeben
 
-const newArray = [];
 regexKonsonantVokal = /^[^AEIOUÄÖÜaeiouäöü][AEIOUÄÖÜaeiouäöü]/; // Erster Buchstabe Konsonant gefolgt von Vokal
 regexKonsonantKonsonant = /^[^AEIOUÄÖÜaeiouäöü][^AEIOUÄÖÜaeiouäöü]/; // Erster und zweiter Buchstabe Konsonant
-regexVokalAnfang = /[^AEIOUÄÖÜaeiouäöü]/; // Erster Buchstabe Vokal
+regexVokalAnfang = /^[AEIOUÄÖÜaeiouäöü]/; // Erster Buchstabe Vokal
 
-[...argv].forEach((Wort) => {
-  //  console.log(Wort.test(/^[^aeiouäöü][aeiouäöü]/));
-  let result = "";
+function checkWord(inputWord) {
   // Konsonant gefolgt von Vokal am Anfang
-  if (regexKonsonantVokal.test(Wort)) {
-    console.log(`Vokal nach Konsonant trifft zu: ${Wort}`);
-
-    const newWord = Wort.split(""); // Wandle String in ein Array aus char
-    let newKonsonant = newWord.shift(); // Erstes Zeichen in newKonsonant
-    newWord.push(newKonsonant); // Erstes Zeichen am Schluss einfügen
-    newWord.push("a");
-    newWord.push("y");
-    console.log(newKonsonant);
-    console.log(newWord);
-    Wort = "";
+  let pigLatinWord = "";
+  if (regexKonsonantVokal.test(inputWord)) {
+    let arrChars = inputWord.split(""); // Wandle String in ein Array aus char
+    let newKonsonant = arrChars.shift(); // Erstes Zeichen in newKonsonant
+    arrChars.push(newKonsonant); // Erstes Zeichen am Schluss einfügen
+    arrChars.push("a");
+    arrChars.push("y");
+    pigLatinWord = arrChars.join("");
   }
   // Zwei Konsonanten am Anfang
-  console.log("Doppelkonsonant: " + Wort);
-  if (regexKonsonantKonsonant.test(Wort)) {
-    console.log(`Zwei Konsonanten trifft zu: ${Wort}`);
-    const newWord = Wort.split(""); // Wandle String in ein Array aus char
-    let newKonsonant = newWord.shift(); // Erstes Zeichen in newKonsonant
-    newWord.push(newKonsonant);
-    newKonsonant = newWord.shift(); // Neues erstes Zeichen in newKonsonant
-    newWord.push(newKonsonant); // und wieder ans Ende.
-    newWord.push("a");
-    newWord.push("y");
-    console.log(newKonsonant);
-    console.log(newWord);
-    Wort = "";
+  if (regexKonsonantKonsonant.test(inputWord)) {
+    let arrChars = inputWord.split(""); // Wandle String in ein Array aus char
+    let newKonsonant = arrChars.shift(); // Erstes Zeichen in newKonsonant
+    arrChars.push(newKonsonant);
+    newKonsonant = arrChars.shift(); // Neues erstes Zeichen in newKonsonant
+    arrChars.push(newKonsonant); // und wieder ans Ende.
+    arrChars.push("a");
+    arrChars.push("y");
+    pigLatinWord = arrChars.join("");
   }
-  console.log("Vokal: " + Wort);
-  if (regexVokalAnfang.test(Wort)) {
-    console.log(`Vokal am Anfang trifft zu: ${Wort}`);
-    const newWord = Wort + "way";
-    console.log(newWord);
+  console.log("Vokal: " + inputWord);
+  if (regexVokalAnfang.test(inputWord)) {
+    pigLatinWord = inputWord + "way";
   }
+  pigLatinWords.push(pigLatinWord);
+}
+
+const argv = process.argv.slice(2); // Entferne zwei Elemente
+// Arg1 = The path to the Node.js executable (your installation of Node)
+// Arg2 = The path to the JavaScript file being executed (your file)
+// Jeder Eintrag ist ein String!
+if (process.argv.length < 3) {
+  console.log("Keine Eingabe gemacht! Bitte erneut versuchen.");
+  process.exit(1);
+}
+
+let pigLatinWords = [];
+const wordArray = argv[0].split(" ");
+// [...argv].forEach((Wort) => {
+[...wordArray].forEach((singleWord) => {
+  checkWord(singleWord);
 });
+console.log(pigLatinWords.join(" "));
