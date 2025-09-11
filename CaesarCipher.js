ef8c547b3d4503ee5524c95026ecc243f3bd657b
Frank Steinmetz <frank.steinmetz@steinmetz-it.de>
2025-09-11 08:29:24 +0200

    Alle Spiele komplettiert

--- CaesarCipher.js
+++ CaesarCipher.js
@@ -1,9 +1,88 @@
 // In progress
-const argv = process.argv.slice(2); // Entferne zwei Elemente
-// Arg1 = The path to the Node.js executable (your installation of Node)
-// Arg2 = The path to the JavaScript file being executed (your file)
-// Jeder Eintrag ist ein String!
+function splashScreen() {
+  console.log(
+    "***********************************************************************"
+  );
+  console.log(
+    "**                                                                    *"
+  );
+  console.log(
+    "**                C  a  e  s  a  r    C  i  p  h  e  r                *"
+  );
+  console.log(
+    "**                                                                    *"
+  );
+  console.log(
+    "***********************************************************************"
+  );
+}
+// const listChars = "abcdefghijklmnopqrstuvwxyzäöüß";
+const listChars = "abcdefghijklmnopqrstuvwxyz";
+const encryptCharArray = listChars.split(""); // String in in Array wandeln.
+let encryptCharString = "";
+let stringCaesarCipher = "";
+const argv = process.argv.slice(2); // Pfad und Dateinamen übergehen.
+splashScreen();
+// Eingabe prüfen
 if (process.argv.length < 3) {
-  console.log("Keine Auswahl getroffen! Bitte erneut versuchen.");
+  console.log("Zuwenig Parameter eingegeben! Bitte Text und Offset eingeben.");
   process.exit(1);
 }
+const shiftFactor = parseInt(argv[1]); // offset
+const origText = argv[0].toLowerCase(); // text to cipher
+
+if (shiftFactor > listChars.length) {
+  console.log("Die Stellenzahl zum verschieben ist zu gross.");
+  process.exit(1);
+}
+
+let shiftCounter = shiftFactor;
+
+if (shiftCounter > 0) {
+  while (shiftCounter > 0) {
+    let firstLetter = encryptCharArray.shift();
+    encryptCharArray.push(firstLetter);
+    shiftCounter--;
+  }
+} else {
+  shiftCounter = Math.abs(shiftCounter);
+  while (shiftCounter > 0) {
+    let lastLetter = encryptCharArray.pop();
+    encryptCharArray.unshift(lastLetter);
+    shiftCounter--;
+  }
+}
+
+encryptCharString = encryptCharArray.join(""); // String mit Versatz
+let charToCipher = "";
+let cipherCharASCII = 0;
+for (let i = 0; i < origText.length; i++) {
+  charToCipher = origText[i];
+  if (charToCipher === " ") {
+    // space
+    cipherCharASCII = " ".charCodeAt(0);
+    stringCaesarCipher += " ";
+  } else {
+    cipherCharASCII = charToCipher.charCodeAt(0); // ASCII Wert des Zeichens
+    if (cipherCharASCII > 200) {
+      switch (cipherCharASCII) {
+        case 228: // ä
+          cipherCharASCII -= 228;
+          break;
+        case 246: // ö
+          cipherCharASCII -= 220;
+          break;
+        case 252: // ü
+          cipherCharASCII -= 252;
+          break;
+        case 223: // ß
+          cipherCharASCII -= 223;
+          break;
+      }
+    } else {
+      cipherCharASCII -= 97; // Wert korrigieren damit er zum Anfang des Strings passt.
+    }
+    stringCaesarCipher += encryptCharString[cipherCharASCII];
+  }
+}
+console.log(stringCaesarCipher);
--- PigLatinTranslator.js
+++ PigLatinTranslator.js
@@ -1,4 +1,4 @@
-// Future Extension
+// Testsatz: There are other Happy fish in the sea Child
 const argv = process.argv.slice(2); // Entferne zwei Elemente
 // Arg1 = The path to the Node.js executable (your installation of Node)
 // Arg2 = The path to the JavaScript file being executed (your file)
@@ -15,4 +15,51 @@ if (process.argv.length < 3) {
 //      Example: Child = Ildch + ay = Ildchay
 // Wenn ein Wort mit einem Vokal beginnt, füge das Wort „way“ am Ende des Wortes hinzu.
 //      Awesome = Awesome +way = Awesomeway
-// Testen??
+
+// Als erstes aus der Eingabe ein Array machen
+// Dann für jedes Wort prüfen ob eine Regel zutrifft. Falls ja anwenden.
+// Zuletzt dann wieder String erstellen und ausgeben
+
+const newArray = [];
+regexKonsonantVokal = /^[^AEIOUÄÖÜaeiouäöü][AEIOUÄÖÜaeiouäöü]/; // Erster Buchstabe Konsonant gefolgt von Vokal
+regexKonsonantKonsonant = /^[^AEIOUÄÖÜaeiouäöü][^AEIOUÄÖÜaeiouäöü]/; // Erster und zweiter Buchstabe Konsonant
+regexVokalAnfang = /[^AEIOUÄÖÜaeiouäöü]/; // Erster Buchstabe Vokal
+
+[...argv].forEach((Wort) => {
+  //  console.log(Wort.test(/^[^aeiouäöü][aeiouäöü]/));
+  let result = "";
+  // Konsonant gefolgt von Vokal am Anfang
+  if (regexKonsonantVokal.test(Wort)) {
+    console.log(`Vokal nach Konsonant trifft zu: ${Wort}`);
+
+    const newWord = Wort.split(""); // Wandle String in ein Array aus char
+    let newKonsonant = newWord.shift(); // Erstes Zeichen in newKonsonant
+    newWord.push(newKonsonant); // Erstes Zeichen am Schluss einfügen
+    newWord.push("a");
+    newWord.push("y");
+    console.log(newKonsonant);
+    console.log(newWord);
+    Wort = "";
+  }
+  // Zwei Konsonanten am Anfang
+  console.log("Doppelkonsonant: " + Wort);
+  if (regexKonsonantKonsonant.test(Wort)) {
+    console.log(`Zwei Konsonanten trifft zu: ${Wort}`);
+    const newWord = Wort.split(""); // Wandle String in ein Array aus char
+    let newKonsonant = newWord.shift(); // Erstes Zeichen in newKonsonant
+    newWord.push(newKonsonant);
+    newKonsonant = newWord.shift(); // Neues erstes Zeichen in newKonsonant
+    newWord.push(newKonsonant); // und wieder ans Ende.
+    newWord.push("a");
+    newWord.push("y");
+    console.log(newKonsonant);
+    console.log(newWord);
+    Wort = "";
+  }
+  console.log("Vokal: " + Wort);
+  if (regexVokalAnfang.test(Wort)) {
+    console.log(`Vokal am Anfang trifft zu: ${Wort}`);
+    const newWord = Wort + "way";
+    console.log(newWord);
+  }
+});
